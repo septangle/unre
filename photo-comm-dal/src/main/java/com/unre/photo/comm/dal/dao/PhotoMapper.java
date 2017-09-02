4a21583df11888e1524d2109e38bcbf1dca2499b
saharazhang <saharazhang@gmail.com>
2017-09-02 15:18:59 +0800

    add photo dal source

---
+++ photo-comm-dal/src/main/java/com/unre/photo/comm/dal/dao/PhotoMapper.java
@@ -0,0 +1,17 @@
+package com.unre.photo.comm.dal.dao;
+
+import com.unre.photo.comm.dal.model.Photo;
+
+public interface PhotoMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Photo record);
+
+    int insertSelective(Photo record);
+
+    Photo selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Photo record);
+
+    int updateByPrimaryKey(Photo record);
+}
---
+++ photo-comm-dal/src/main/java/com/unre/photo/comm/dal/model/Photo.java
@@ -0,0 +1,105 @@
+package com.unre.photo.comm.dal.model;
+
+import java.util.Date;
+
+public class Photo {
+    private Long id;
+
+    private Long orderId;
+
+    private Long panoramaId;
+
+    private String imagePath;
+
+    private String thumbImagePath;
+
+    private String isDeleted;
+
+    private Integer createBy;
+
+    private Date createTime;
+
+    private Integer updateBy;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Long getPanoramaId() {
+        return panoramaId;
+    }
+
+    public void setPanoramaId(Long panoramaId) {
+        this.panoramaId = panoramaId;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath == null ? null : imagePath.trim();
+    }
+
+    public String getThumbImagePath() {
+        return thumbImagePath;
+    }
+
+    public void setThumbImagePath(String thumbImagePath) {
+        this.thumbImagePath = thumbImagePath == null ? null : thumbImagePath.trim();
+    }
+
+    public String getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(String isDeleted) {
+        this.isDeleted = isDeleted == null ? null : isDeleted.trim();
+    }
+
+    public Integer getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Integer createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(Integer updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
