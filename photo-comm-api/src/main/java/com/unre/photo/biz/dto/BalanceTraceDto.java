11ec7dd5c465d5e4e81af00533aeb60019b5eb6f
saharazhang <saharazhang@gmail.com>
2017-09-08 19:04:31 +0800

    update

---
+++ photo-comm-api/src/main/java/com/unre/photo/biz/dto/BalanceTraceDto.java
@@ -0,0 +1,136 @@
+package com.unre.photo.biz.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class BalanceTraceDto {
+	private Long id;
+
+    private Long memberId;
+    
+    private Long transNo;
+
+    private String transType;
+
+    private BigDecimal amount;
+
+    private BigDecimal balance;
+
+    private String status;
+
+    private String remark;
+
+    private Long version;
+
+    private Integer createBy;
+
+    private Date createTime;
+
+    private Integer updateBy;
+
+    private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public Long getTransNo() {
+        return transNo;
+    }
+
+    public void setTransNo(Long transNo) {
+        this.transNo = transNo;
+    }
+
+    public String getTransType() {
+        return transType;
+    }
+
+    public void setTransType(String transType) {
+        this.transType = transType == null ? null : transType.trim();
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public Integer getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Integer createBy) {
+        this.createBy = createBy;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(Integer updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
